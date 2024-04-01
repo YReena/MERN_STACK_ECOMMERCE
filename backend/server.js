2144a0c7fd9c5b86c747046bb800f688db824552
Reena Yadav <yadav@1993reena.com>
2024-04-01 22:19:54 +0530

    fouth commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const dotenv= require("dotenv");
-const path = require("../backend/config/config.env");
+const path = require("path");
 const cloudinary = require('cloudinary');
 
 
@@ -18,6 +18,13 @@ cloudinary.config({
   api_key: "193181991474174",
   api_secret: "vBwyL4zxAElrn-buiLHTkuY0C7k",
 });
+
+app.use(express.static(path.join(__dirname,"./backend/build")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./backend/build"));
+})
+
 
 require('../backend/config/database');
 
